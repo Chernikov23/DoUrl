e4c53135d6bf834bcca0c4f98685c1c083d13873
Alexander <118567820+Chernikov23@users.noreply.github.com>
2025-01-11 21:48:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pyshorteners
 import os 
 import logging
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
@@ -17,15 +18,51 @@ logger = logging.getLogger(__name__)
 logger.info('Бот запущен и работает...')
 
 
+USER_DATA_FILE = "users.json"
+
+def load_users():
+    if os.path.exists(USER_DATA_FILE):
+        try:
+            with open(USER_DATA_FILE, "r") as file:
+                return json.load(file)
+        except json.JSONDecodeError:
+            print("Ошибка чтения файла. Создается пустой файл.")
+    return []
+
+def add_user(user_id: int, balance: int = 0, is_sub: bool = False):
+    users = load_users()
+    if any(user['id'] == user_id for user in users):
+        print(f"Пользователь с ID {user_id} уже существует.")
+        return
+    users.append({'id': user_id, 'balance': balance, 'is_sub': is_sub})
+    save_users(users)
+    print(f"Пользователь с ID {user_id} успешно добавлен.")
+
+def amount():
+    users = load_users()
+    lst = []
+    lst.append(user['id'] for user in users)
+    return len(lst)
+
+def save_users(data):
+    with open(USER_DATA_FILE, "w") as file:
+        json.dump(data, file)
+
+
 def shorten_url(url):
     try:
         return pyshorteners.Shortener().clckru.short(url)
     except Exception as e:
         return "Введите корректную ссылку"
 
+@dp.message(Command('amount'))
+async def amount_users(msg: Message):
+    await msg.answer(f"Кол-во пользователей: {amount()}")
 
 @dp.message(Command('start'))
 async def start(msg: Message):
+    user_id = msg.from_user.id
+    add_user(user_id)
     await msg.answer('Привет! Я бот для сокращения ссылок. Можешь использовать inline режим, чтобы сократить ссылку в любом чате\n\nЧтобы сократить ссылку введите: @dourlbot ссылка')
 
 @dp.message()
